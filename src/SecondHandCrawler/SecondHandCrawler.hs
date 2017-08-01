e2124983b869eae36941287d37073ca5253d54ac
yannick-cw <yannick.gladow@googlemail.com>
2017-08-01 20:46:05 +0200

    Adds image src to extraction

--- src/SecondHandCrawler/SecondHandCrawler.hs
+++ src/SecondHandCrawler/SecondHandCrawler.hs
@@ -8,9 +8,19 @@ module SecondHandCrawler.SecondHandCrawler
 import           Control.Applicative
 import           Text.HTML.Scalpel
 
-newtype SecondHandOffer = SecondHandOffer
-  { title :: String
+data SecondHandOffer = SecondHandOffer
+  { title  :: String
+  , imgSrc :: String
   } deriving (Eq, Show)
 
-parseHtml :: String -> [SecondHandOffer]
-parseHtml html = [SecondHandOffer "testOffer"]
+parseHtml :: String -> Maybe [SecondHandOffer]
+parseHtml html = scrapeStringLike html secondHandOffer
+   where
+   secondHandOffer :: Scraper String [SecondHandOffer]
+   secondHandOffer = chroots ("div" @: [hasClass "gm_offer"]) offer
+
+   offer :: Scraper String SecondHandOffer
+   offer = do
+        title      <- text $ "a" @: [hasClass "dtl"]
+        imageSrc <- attr "src" "img"
+        return $ SecondHandOffer title imageSrc
--- test/SecondHandCrawler/SecondHandParserSpec.hs
+++ test/SecondHandCrawler/SecondHandParserSpec.hs
@@ -4,27 +4,29 @@ import           SecondHandCrawler.SecondHandCrawler (SecondHandOffer (..),
                                                       parseHtml)
 import           Test.Hspec
 
-htmlString :: String
-htmlString =
-  "<html>\
-\  <body>\
-\  <div class='gm_offer' id='gm_offer_id_58952'>\
+htmlString :: String -> String
+htmlString divs = "<html><body>" ++ divs ++ "</body></html>"
+
+offerHtml =
+  "  <div class='gm_offer' id='gm_offer_id_58952'>\
 \      <div class='gm_offer_image '>\
 \          <a href='/db3/gebrauchtmarkt/anzeige/id/58952' title='Mntor 2 M'>\
-\     <img src='/dbfiles/gm/images/2017/08/thumbnail/15016070316652img58952.jpg' alt='&raquo; Details' title='Mntor 2 M' />\
+\     <img src='/src.jpg' alt='&raquo; Details' title='Mntor 2 M' />\
 \     </a>\
 \      </div>\
 \      <div class='gm_offer_description '>\
-\          <h2><a href='/db3/gebrauchtmarkt/anzeige/id/58952' title='Zu den Angebot Details' class = 'dtl'>Mentor 3 M |  Check neu/ Tuch neuwertig</a></h2>\
+\          <h2><a href='/db3/gebrauchtmarkt/anzeige/id/58952' title='Zu den Angebot Details' class = 'dtl'>Mentor for test</a></h2>\
 \          <div class='bodytext'>\
-\              Gepflegten Mentor 3 M zu verkaufen. Der Schirm wurde im Juni 2017 gecheckt: Tuch neuwertig!  Der Schirm hat eine kleine Flickstelle im Segel (Rauch...\
+\              TestDescription\
 \          </div>\
 \      </div>\
-\    </div>\
-\  </body>\
-\</html>"
+\    </div>"
 
 spec :: Spec
 spec =
   describe "SecondHandParser" $ do
-    it "should parse html as string to result datatype" $ parseHtml htmlString `shouldBe` [SecondHandOffer "testOffer"]
+    it "should parse html as string to result datatype" $ parseHtml (htmlString offerHtml) `shouldBe` Just [offer]
+    it "should parse html as string to result datatype" $
+      parseHtml (htmlString $ offerHtml ++ offerHtml) `shouldBe` Just [offer, offer]
+  where
+    offer = SecondHandOffer {title = "Mentor for test", imgSrc = "/src.jpg"}
